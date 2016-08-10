c58c3022886672d89851970cd2267cf35f1d54f0
Margus Pärt <margus@wave.ee>
2016-08-10 13:42:03 +0300

    added 2my1b1j files

---
+++ test/analyze_2mu1b1j_cfg.py
@@ -0,0 +1,34 @@
+import FWCore.ParameterSet.Config as cms
+
+import os
+
+process = cms.PSet()
+
+process.fwliteInput = cms.PSet(
+    fileNames = cms.vstring('/afs/cern.ch/user/v/veelken/scratch0/VHbbNtuples_7_6_x/CMSSW_7_6_3/src/VHbbAnalysis/Heppy/test/latest_Loop/tree.root'),
+    ##fileNames = cms.vstring('/afs/cern.ch/user/k/kaehatah/public/ttHJetToNonbb_M125_13TeV_amcatnloFXFX_madspin_pythia8_mWCutfix/VHBB_HEPPY_V12_ttHJetToNonbb_M125_13TeV_amcatnloFXFX_madspin_Py8_mWCutfix__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160330_172426/0000/tree_1.root'),
+    maxEvents = cms.int32(-1),
+    outputEvery = cms.uint32(100000)
+)
+
+process.fwliteOutput = cms.PSet(
+    fileName = cms.string('analyze_2mu1b1j.root')
+)
+
+process.analyze_2mu1b1j = cms.PSet(
+    treeName = cms.string('tree'),
+
+    process = cms.string('data_obs'),
+
+    triggers_1mu = cms.vstring("HLT_BIT_HLT_IsoMu20_v", "HLT_BIT_HLT_IsoTkMu20_v"),
+    use_triggers_1mu = cms.bool(True),
+    triggers_2mu = cms.vstring("HLT_BIT_HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v", "HLT_BIT_HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v"),
+    use_triggers_2mu = cms.bool(True),
+
+    isMC = cms.bool(False),
+    central_or_shift = cms.string('central'),
+    lumiScale = cms.double(1.),
+
+    selEventsFileName_input = cms.string(''),
+    selEventsFileName_output = cms.string('')
+)
