83c87f3f323994473abd8b5aaeaf43b0acdce005
Margus Pärt <margus@wave.ee>
2016-10-11 16:33:22 +0300

    fix

--- python/tthAnalyzeSamples_2mu1b1j_2015.py
+++ python/tthAnalyzeSamples_2mu1b1j_2015.py
@@ -3,14 +3,16 @@ from collections import OrderedDict as OD
 from tthAnalysis.HiggsToTauTau.tthAnalyzeSamples_2015 import samples_2015
 
 
-for sample_name, sample_info in samples_2015.items():
-  # if sample_info["type"] == "mc":
-  #    sample_info["triggers"] = [ "1e", "1mu" ]
+allowed_datasets = [
+    "/DoubleMuon/Run2015C_25ns-16Dec2015-v1/MINIAOD",
+    "/DoubleMuon/Run2015D-16Dec2015-v1/MINIAOD",
+    "/SingleMuon/Run2015C_25ns-16Dec2015-v1/MINIAOD",
+    "/SingleMuon/Run2015D-16Dec2015-v1/MINIAOD"
+]
 
-  if sample_name not in [
-      "/DoubleMuon/Run2015C_25ns-16Dec2015-v1/MINIAOD",
-      "/DoubleMuon/Run2015D-16Dec2015-v1/MINIAOD",
-      "/SingleMuon/Run2015C_25ns-16Dec2015-v1/MINIAOD",
-      "/SingleMuon/Run2015D-16Dec2015-v1/MINIAOD"
-    ]
-    sample_info["use_it"] = False
+for sample_name, sample_info in samples_2015.items():
+    # if sample_info["type"] == "mc":
+    #    sample_info["triggers"] = [ "1e", "1mu" ]
+
+    if sample_name not in allowed_datasets:
+        sample_info["use_it"] = False
