6d1a28017bce516cef6e954f5dfe143a95545c1a
Margus Pxrt <margus@wave.ee>
2016-11-03 22:44:24 +0200

    save

--- macros/create_roofit_plots.cpp
+++ macros/create_roofit_plots.cpp
@@ -132,7 +132,7 @@ bool createRooFit(
   RooRealVar sigma1("breitWigner sigma", "breitWigner sigma", 0.01, 0.01, 10);
   RooBreitWigner model1("breitWigner", "breitWigner", x, mean1, sigma1);
 
-  RooRealVar  mean2("gauss mean", "gauss mean", 0.001);
+  RooRealVar  mean2("gauss mean", "gauss mean", 0);
   RooRealVar  sigma2("gauss sigma", "gauss sigma", 1, 0.01, 10);
   RooGaussian model2("gauss", "gauss", x, mean2, sigma2);
 
