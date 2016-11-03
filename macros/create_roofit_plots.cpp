c95b50f22e1cc927be6d2e27aa93bcca2001dc23
Margus Pxrt <margus@wave.ee>
2016-11-03 02:59:36 +0200

    save

--- macros/create_roofit_plots.cpp
+++ macros/create_roofit_plots.cpp
@@ -50,7 +50,7 @@ bool createRooFit(TH1F *h1, std::string name) {
   sigma.Print();
   model.Print();
 
-  model.fitTo(x);
+  model.fitTo(dataHist);
 
   std::cout << "After fit. \n";
   mean.Print();
