fece7ac3ddb15c87f1f46a22524d16e7d5a1761b
Margus Pxrt <margus@wave.ee>
2016-11-03 04:11:31 +0200

    save

--- macros/create_roofit_plots.cpp
+++ macros/create_roofit_plots.cpp
@@ -36,8 +36,8 @@ bool create_roofit_plots()
   };
 
   for (string categoryName : categoryNames) {
-    TH1F *histogram = loadTH1F(categoryName);
-    return createRooFit(histogram, categoryName);
+    TH1F *histogram = loadTH1F(categoryName.data());
+    return createRooFit(histogram, categoryName.data());
   }
 }
 
