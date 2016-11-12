16aeedfe3edf3fe2633368ac94a1a60d93308051
Margus Pxrt <margus@wave.ee>
2016-11-12 05:05:11 +0200

    background  test

--- macros/create_roofit_plots.cpp
+++ macros/create_roofit_plots.cpp
@@ -156,10 +156,10 @@ bool createRooFit(
   // RooRealVar lambda("lambda", "slope", -1.0, -100.0, 100.0);
   // RooExponential background("expo", "exponential PDF", x, lambda);
 
-  RooRealVar backgroundA("backgroundA", "backgroundA");
-  RooRealVar backgroundB("backgroundB", "backgroundB");
-  RooRealVar backgroundC("backgroundC", "backgroundC");
-  RooRealVar backgroundX("backgroundX", "backgroundX");
+  RooRealVar backgroundA("backgroundA", "backgroundA", 1.1);
+  RooRealVar backgroundB("backgroundB", "backgroundB", 1.1);
+  RooRealVar backgroundC("backgroundC", "backgroundC", 1.1);
+  RooRealVar backgroundX("backgroundX", "backgroundX", 1.1);
 
   RooAbsPdf *background = RooClassFactory::makePdfInstance(
     "background",
