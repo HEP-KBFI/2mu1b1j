e34fc2d34f342ec61be17f981f924810eb47295f
Margus Pärt <margus@wave.ee>
2016-11-17 12:20:19 +0200

    no old data

--- macros/run_macros.sh
+++ macros/run_macros.sh
@@ -1,6 +1,6 @@
 git pull
+rm -rf /home/margusp/roofits/
 mkdir /home/margusp/roofits/
-rm /home/margusp/roofits/*
 
 root -q -b create_roofit_plots.cpp | grep pValue > out-pvalue.log
 cp *.log /home/margusp/roofits/
