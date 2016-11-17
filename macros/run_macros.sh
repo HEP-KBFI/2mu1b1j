08ab0fbea2e8e9bb91b02d18171e8fb26e6ff932
Margus Pärt <margus@wave.ee>
2016-11-17 12:23:38 +0200

    changed log output

--- macros/run_macros.sh
+++ macros/run_macros.sh
@@ -2,5 +2,4 @@ git pull
 rm -rf /home/margusp/roofits/
 mkdir /home/margusp/roofits/
 
-root -q -b create_roofit_plots.cpp | grep pValue > out-pvalue.log
-cp *.log /home/margusp/roofits/
+root -q -b create_roofit_plots.cpp | grep pValue > /home/margusp/roofits/out-pvalue.log
