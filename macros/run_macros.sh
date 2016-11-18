f8466de6896b71fe4a69e8261f93b9fa7f110c0c
Margus Pärt <margus@wave.ee>
2016-11-18 18:23:19 +0200

    save

--- macros/run_macros.sh
+++ macros/run_macros.sh
@@ -1,6 +1,11 @@
+
+
 git pull
 rm -rf /home/margusp/roofits/
 mkdir /home/margusp/roofits/
+
+root-config --cflags
+root-config --glibs
 
 make
 ./build/app.exe | grep pValue > /home/margusp/roofits/out-pvalue.log
