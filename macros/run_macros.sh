0e710c6a00601d51f9c6512ee568600dda1bb1e0
Margus Pxrt <margus@wave.ee>
2017-05-09 04:19:06 +0300

    restored old working version

--- macros/run_macros.sh
+++ macros/run_macros.sh
@@ -1,14 +1,6 @@
 
-
 git pull
 rm -rf /home/margusp/roofits/
 mkdir /home/margusp/roofits/
 
-#root-config --cflags
-#root-config --glibs
-
-#make
-#./build/app.exe | grep pValue > /home/margusp/roofits/out-pvalue.log
-
-
-g++ -Wall -O2 
+root -q -b create_roofit_plots.cpp | grep pValue > /home/margusp/roofits/out-pvalue.log
